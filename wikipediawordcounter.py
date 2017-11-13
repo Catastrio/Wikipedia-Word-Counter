9c872bd4565876695b0d6deffdc32532e61f7baa
Catastrio <catastrio@gmail.com>
2017-11-13 09:22:21 -0700

    Setting up the repository.

---
+++ wikipediawordcounter.py
@@ -0,0 +1,7 @@
+# File:             wikpediawordcounter.py
+# Author:           Michael Cassio
+# Date:             11/13/17
+# E-mail:           catastrio@gmail.com
+# Description:      A program that will grab the contents of a Wikipedia page
+#                   then count the number of times a specific word appears.
+
