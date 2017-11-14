f2b1805ce5291bd51f1a1ae6dda40c61a716afc2
Catastrio <catastrio@gmail.com>
2017-11-14 05:11:42 -0700

    User inputs Wikipedia article & text from first paragraph returns.
    The process is fairly simple. As of right now, if you input a Wikipedia
    article, only the top summary is returned.
    
    Still working on figuring out the full article but this is fairly simple
    and straightforward.

--- wikipediawordcounter.py
+++ wikipediawordcounter.py
@@ -1,7 +1,19 @@
 # File:             wikpediawordcounter.py
 # Author:           Michael Cassio
-# Date:             11/13/17
+# Date:             11/14/17
 # E-mail:           catastrio@gmail.com
 # Description:      A program that will grab the contents of a Wikipedia page
 #                   then count the number of times a specific word appears.
 
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    
+    userURL = input("Please enter a Wikipedia URL to download: ")
+
+    page = requests.get(userURL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print(soup.find_all('p')[0].get_text())
+
+main()
