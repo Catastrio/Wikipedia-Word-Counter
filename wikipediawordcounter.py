450fcd1824ea09be5e572af01382bf5bc09e7233
Catastrio <catastrio@gmail.com>
2017-11-14 15:29:40 -0700

    Complete Change - New Foundation
    After extensive testing of different systems, I think I finally have a
    solid grasp on how to scrape the contents of a Wikipedia page.
    
    There's other methods and libraries to do it but I did mine as bare
    bones as possible.
    
    In the future, I think I want to find a way to remove the footnotes so
    the article doesn't have [1] [2] and so on next to different sentences.
    
    This code is now the foundation going forward on this project. Will try
    to commit often so I don't break.

--- wikipediawordcounter.py
+++ wikipediawordcounter.py
@@ -9,11 +9,20 @@ from bs4 import BeautifulSoup
 import requests
 
 def main():
-    
-    userURL = input("Please enter a Wikipedia URL to download: ")
 
-    page = requests.get(userURL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup.find_all('p')[0].get_text())
+    import requests
+    from bs4 import BeautifulSoup
+
+    number = 0
+
+    userInput = input('Please input a Wikipedia article URL. Try "http://en.wikipedia.org/wiki/Special:Random" to get a random article!: ')
+    req = requests.get(userInput)
+    soup = BeautifulSoup(req.text, "lxml")
+
+    length = len(soup.find_all('p'))
+
+    while(number < length):
+        print(soup.find_all('p')[number].get_text())
+        number += 1
 
 main()
