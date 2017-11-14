89076765f63b62863e91932dbe7399a177dc6d1c
Catastrio <catastrio@gmail.com>
2017-11-14 16:20:12 -0700

    Cleaned up input asks, can now word count based on input
    The program will ask the user for a Wikipedia article + a word then dumb
    out the entire article and the number of times their word appears.
    
    Dumping out the article is probably unnecessary but this was just to
    keep things clean
    
    For future reference: The word count isn't TOTALLY true. It counts how
    many times the word appears in the content of the articles. Any time the
    word appears under a photo or in links in the footnotes or places
    outside of the main article, it's not counted.
    
    In other words, using Ctrl + F to count the number of times a word
    appears in the article and comparing that to the output of my program
    will likely almost never match.

--- wikipediawordcounter.py
+++ wikipediawordcounter.py
@@ -14,15 +14,24 @@ def main():
     from bs4 import BeautifulSoup
 
     number = 0
+    wordstring = ''
 
-    userInput = input('Please input a Wikipedia article URL. Try "http://en.wikipedia.org/wiki/Special:Random" to get a random article!: ')
-    req = requests.get(userInput)
+    userArticle = input('Please input a Wikipedia article URL. Try "http://en.wikipedia.org/wiki/Special:Random" to get a random article!: ')
+    userWord = input('Please enter a word to get the word count for (THIS IS CASE SENSITIVE): ')
+    
+    req = requests.get(userArticle)
     soup = BeautifulSoup(req.text, "lxml")
 
     length = len(soup.find_all('p'))
 
     while(number < length):
+        wordstring  += soup.find_all('p')[number].get_text()
         print(soup.find_all('p')[number].get_text())
         number += 1
+
+    wordlist = wordstring.split()
+    wordfreq = [wordlist.count(w) for w in wordlist]
+    
+    print('\nThe word you entered: "' + userWord + '" appears ' + str(wordlist.count(userWord)) + ' times.')
 
 main()
